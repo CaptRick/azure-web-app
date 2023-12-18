8897f07559bb52a20304b18ba3e6aba5bbf7aab7
Ashbeel Ghouri <mailto:ashbeel@focusteck.com>
2023-12-18 23:12:17 +0500

    card game added

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
 
-// app.get('/', (req, res) => {
-//   res.send('Hello, Azure!');
-// });
-
 const { BlobServiceClient } = require('@azure/storage-blob');
 const connectionString = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=azurewebappstorage1;AccountKey=4xorNCdRBEl491N0UppjPGB7nfGC57LsVl/PxXYIWpDpbe0plsE7RE+O+tcB5arpEe3Wj9X9nGgo+AStrjtL3Q==;BlobEndpoint=https://azurewebappstorage1.blob.core.windows.net/;FileEndpoint=https://azurewebappstorage1.file.core.windows.net/;QueueEndpoint=https://azurewebappstorage1.queue.core.windows.net/;TableEndpoint=https://azurewebappstorage1.table.core.windows.net/";
 const containerName = 'static';
---
+++ public/script.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const symbols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+    const cards = [...symbols, ...symbols]; // Duplicate symbols for matching pairs
+    let flippedCards = [];
+
+    const memoryGame = document.querySelector('.memory-game');
+
+    // Shuffle the cards array
+    cards.sort(() => Math.random() - 0.5);
+
+    // Create card elements dynamically
+    cards.forEach((symbol, index) => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.dataset.symbol = symbol;
+        card.addEventListener('click', flipCard);
+        
+        const cardContent = document.createElement('div');
+        cardContent.textContent = symbol;
+
+        card.appendChild(cardContent);
+        memoryGame.appendChild(card);
+    });
+
+    function flipCard() {
+        if (flippedCards.length < 2 && !this.classList.contains('flipped')) {
+            this.classList.add('flipped');
+            flippedCards.push(this);
+
+            if (flippedCards.length === 2) {
+                setTimeout(checkMatch, 500);
+            }
+        }
+    }
+
+    function checkMatch() {
+        const [card1, card2] = flippedCards;
+        
+        if (card1.dataset.symbol === card2.dataset.symbol) {
+            card1.classList.add('matched');
+            card2.classList.add('matched');
+        } else {
+            card1.classList.remove('flipped');
+            card2.classList.remove('flipped');
+        }
+
+        flippedCards = [];
+
+        // Check for win
+        if (document.querySelectorAll('.matched').length === cards.length) {
+            alert('Congratulations! You matched all pairs!');
+        }
+    }
+});
