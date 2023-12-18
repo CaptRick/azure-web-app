821e6d07d616c53a031ababa8db2fe31b029e34c
Ashbeel Ghouri <mailto:ashbeel@focusteck.com>
2023-12-18 23:53:03 +0500

    alphabets game added

--- public/script.js
+++ public/script.js
@@ -1,37 +1,56 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const molesContainer = document.getElementById('moles-container');
+    const fallingAlphabetsContainer = document.getElementById('falling-alphabets');
     const scoreElement = document.getElementById('score');
     let score = 0;
 
-    molesContainer.addEventListener('click', (event) => {
-        if (event.target.classList.contains('mole')) {
-            score++;
-            updateScore();
-            hideMole(event.target);
-        }
+    // Create an array of alphabets
+    const alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    function createFallingAlphabet() {
+        const fallingAlphabet = document.createElement('div');
+        fallingAlphabet.classList.add('falling-alphabet');
+        fallingAlphabet.style.left = `${Math.random() * window.innerWidth}px`;
+        fallingAlphabet.textContent = getRandomAlphabet();
+        fallingAlphabetsContainer.appendChild(fallingAlphabet);
+
+        // Move the falling alphabet downward
+        let top = 0;
+        const intervalId = setInterval(() => {
+            top += 5;
+            fallingAlphabet.style.top = `${top}px`;
+
+            // Check if the falling alphabet reached the bottom
+            if (top > window.innerHeight) {
+                clearInterval(intervalId);
+                fallingAlphabetsContainer.removeChild(fallingAlphabet);
+            }
+        }, 20);
+    }
+
+    function getRandomAlphabet() {
+        const randomIndex = Math.floor(Math.random() * alphabets.length);
+        return alphabets[randomIndex];
+    }
+
+    document.addEventListener('keydown', (event) => {
+        const pressedKey = event.key.toUpperCase();
+        const fallingAlphabets = document.querySelectorAll('.falling-alphabet');
+
+        fallingAlphabets.forEach((fallingAlphabet) => {
+            if (fallingAlphabet.textContent === pressedKey) {
+                score++;
+                updateScore();
+                fallingAlphabetsContainer.removeChild(fallingAlphabet);
+            }
+        });
     });
 
     function updateScore() {
         scoreElement.textContent = `Score: ${score}`;
     }
 
-    function showMole() {
-        const mole = document.createElement('div');
-        mole.classList.add('mole');
-        mole.style.top = `${Math.random() * 300}px`;
-        mole.style.left = `${Math.random() * 300}px`;
-        molesContainer.appendChild(mole);
-
-        setTimeout(() => {
-            hideMole(mole);
-        }, 1000 + Math.random() * 2000); // Moles appear for 1-3 seconds
-    }
-
-    function hideMole(mole) {
-        molesContainer.removeChild(mole);
-        showMole(); // Show a new mole after hiding the previous one
-    }
-
-    // Start the game by showing the first mole
-    showMole();
+    // Start the game by creating falling alphabets
+    setInterval(() => {
+        createFallingAlphabet();
+    }, 1000);
 });
