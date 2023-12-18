66553f1ae243048d00af1fd0c1f5d689d0f3af0b
Ashbeel Ghouri <mailto:ashbeel@focusteck.com>
2023-12-18 23:46:14 +0500

    pacman game added

--- public/script.js
+++ public/script.js
@@ -1,110 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
     const board = document.getElementById('game-board');
-    let snake = [{ x: 10, y: 10 }]; // Initial snake position
-    let food = getRandomPosition();
-    let direction = 'right';
-    let intervalId;
+    const pacman = createEntity('pacman', 1, 1);
+    const ghosts = [createEntity('ghost', 10, 10), createEntity('ghost', 15, 5)];
 
-    function getRandomPosition() {
-        return {
-            x: Math.floor(Math.random() * 20),
-            y: Math.floor(Math.random() * 20)
-        };
-    }
-
-    function draw() {
-        board.innerHTML = '';
-
-        // Draw snake
-        snake.forEach(segment => {
-            const snakeElement = document.createElement('div');
-            snakeElement.classList.add('snake');
-            snakeElement.style.gridRow = segment.y + 1;
-            snakeElement.style.gridColumn = segment.x + 1;
-            board.appendChild(snakeElement);
-        });
-
-        // Draw food
-        const foodElement = document.createElement('div');
-        foodElement.classList.add('food');
-        foodElement.style.gridRow = food.y + 1;
-        foodElement.style.gridColumn = food.x + 1;
-        board.appendChild(foodElement);
-    }
-
-    function move() {
-        const head = Object.assign({}, snake[0]);
-
-        switch (direction) {
-            case 'up':
-                head.y--;
-                break;
-            case 'down':
-                head.y++;
-                break;
-            case 'left':
-                head.x--;
-                break;
-            case 'right':
-                head.x++;
-                break;
-        }
-
-        if (head.x < 0) head.x = 19;
-        if (head.x > 19) head.x = 0;
-        if (head.y < 0) head.y = 19;
-        if (head.y > 19) head.y = 0;
-
-        if (isSnakeCollision(head)) {
-            gameOver();
-            return;
-        }
-
-        snake.unshift(head);
-
-        if (head.x === food.x && head.y === food.y) {
-            food = getRandomPosition();
-        } else {
-            snake.pop();
-        }
-
-        draw();
-    }
-
-    function isSnakeCollision(head) {
-        return snake.some(segment => segment.x === head.x && segment.y === head.y);
-    }
-
-    function resetGame() {
-        snake = [{ x: 10, y: 10 }];
-        food = getRandomPosition();
-        direction = 'right';
-        clearInterval(intervalId);
-        intervalId = setInterval(move, 200);
-        draw();
-    }
-
-    function gameOver() {
-        alert('Game Over!');
-        resetGame();
-    }
+    board.appendChild(pacman);
+    ghosts.forEach(ghost => board.appendChild(ghost));
 
     document.addEventListener('keydown', (event) => {
-        switch (event.key) {
-            case 'ArrowUp':
-                direction = 'up';
-                break;
-            case 'ArrowDown':
-                direction = 'down';
-                break;
-            case 'ArrowLeft':
-                direction = 'left';
-                break;
-            case 'ArrowRight':
-                direction = 'right';
-                break;
-        }
+        moveEntity(pacman, event.key);
     });
 
-    resetGame(); // Start the game
+    setInterval(() => {
+        ghosts.forEach(ghost => moveEntity(ghost, getRandomDirection()));
+    }, 1000);
+
+    function createEntity(className, x, y) {
+        const entity = document.createElement('div');
+        entity.className = className;
+        entity.style.gridRow = y + 1;
+        entity.style.gridColumn = x + 1;
+        return entity;
+    }
+
+    function moveEntity(entity, direction) {
+        let x = parseInt(entity.style.gridColumn) - 1;
+        let y = parseInt(entity.style.gridRow) - 1;
+
+        switch (direction) {
+            case 'ArrowUp':
+                y = Math.max(0, y - 1);
+                break;
+            case 'ArrowDown':
+                y = Math.min(19, y + 1);
+                break;
+            case 'ArrowLeft':
+                x = Math.max(0, x - 1);
+                break;
+            case 'ArrowRight':
+                x = Math.min(19, x + 1);
+                break;
+        }
+
+        entity.style.gridRow = y + 1;
+        entity.style.gridColumn = x + 1;
+    }
+
+    function getRandomDirection() {
+        const directions = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
+        return directions[Math.floor(Math.random() * directions.length)];
+    }
 });
